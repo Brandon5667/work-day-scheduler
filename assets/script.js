2ae5409240bb29ec27fd3e3af9ae13c2883cf13c
Brandon Cullifer <brandon.cullifer@gmail.com>
2023-02-26 10:20:26 -0500

    created variables

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,24 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var dateEl = $('#currentDay');
+var save = window.localStorage;
+var container = $(".container-fluid")
+var hours = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
+console.log(hours);
+var day = dayjs().format('ddd, MMM D');
+dateEl.text(day);
+var time = dayjs().format('H');
+console.log(time)
+
+function createTimeBlocks (){
+    for (var i = 0; i<hours.length; i++) {
+      container.append('<div id="hour-'+ hours[i]+'"class="row time-block '+blockChecker(hours[i]) +'"><div class="col-2 col-md-1 hour text-center py-3">'+ fixTime(hours[i]) + '</div><textarea class="col-8 col-md-10 description" rows="3" id="'+ hours[i]+' text"> </textarea><button class="btn saveBtn col-2 col-md-1" aria-label="save"><i class="fas fa-save" aria-hidden="true" onclick="save('+hours[i]+', '+$('#'+hours[i]+' text').text()+')"></i></button></div>')
+      
+    };
+}
+createTimeBlocks();
+
 $(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
