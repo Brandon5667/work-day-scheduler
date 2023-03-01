bf551600370750de75d39a15bc5409ecb1dbf9d9
Brandon Cullifer <brandon.cullifer@gmail.com>
2023-02-28 21:49:53 -0500

    fixed storage check

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,20 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 var dateEl = $('#currentDay');
-var save = window.localStorage;
 var container = $(".container-fluid")
 var hours = [8,9,10,11,12,13,14,15,16,17];
-console.log(hours);
 var day = dayjs().format('ddd, MMM D');
 dateEl.text(day);
-// var time = dayjs().format('H');
-var time = 14;
-console.log(time);
+var time = dayjs().format('H');
+// testing variable set with 24hour time to manually set time
+// var time = 14;
+console.log(hours);
 
 
-
+// uses jquery to create time blocks and sets id based on hours array
 function createTimeBlocks (){
     for (var i = 0; i<hours.length; i++) {
       container.append(`<div id="hour-${hours[i]}" class="row time-block ${blockChecker(hours[i])}">
   <div class="col-2 col-md-1 hour text-center py-3">${fixTime(hours[i])}</div>
-  <textarea class="col-8 col-md-10 description" rows="3" id="text${hours[i]}"></textarea>
+  <textarea class="col-8 col-md-10 description" rows="3" id="text${hours[i]}">${checkStorage(hours[i])}</textarea>
   <button class="btn saveBtn col-2 col-md-1" aria-label="save" onclick="saveToDo(${hours[i]})">
     <i class="fas fa-save" aria-hidden="true" id="button${hours[i]}" ></i>
   </button>
@@ -27,8 +23,19 @@ function createTimeBlocks (){
     
     };
 }
+
 createTimeBlocks();
 
+function checkStorage (index){
+  console.log(localStorage.getItem(index));
+
+  if (localStorage.getItem(index) != null){
+    var storedItem = localStorage.getItem(index);
+    console.log(storedItem);
+    return storedItem;
+  }
+}
+// fixes time to 12 hour day
 function fixTime(blockHour){
     var correctTime;
     if (blockHour < 12 && blockHour != 0){
@@ -44,7 +51,7 @@ function fixTime(blockHour){
     return correctTime;
     
 }
-
+// gives blocks a class based on relation to current hour
 function blockChecker(blockHour){
     var blockColor;
     if (blockHour < time){
@@ -58,7 +65,7 @@ function blockChecker(blockHour){
     }
     return blockColor;
 }
-
+// saves text in forms to localStorage if data already exist it removes data from form and localStorage
 function saveToDo(form){
   if (localStorage.getItem(form) == null){
     console.log(form);
@@ -66,30 +73,17 @@ function saveToDo(form){
     console.log(text);
     window.localStorage.setItem(form, text);
     console.log(localStorage.getItem(form));
+    $( "#button" + form).removeClass( "fas fa-save" );
+    $( "#button" + form).addClass( "fas fa-trash" );
   }
   else{
-    $('#text' + form).val("")
-    localStorage.removeItem(form)
+    $( "#button" + form).removeClass( "fas fa-trash" );
+    $( "#button" + form).addClass( "fas fa-save" );
+    $('#text' + form).val("");
+    localStorage.removeItem(form);
+    
   }
   }
 
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
+  
+// class="bi bi-trash"
